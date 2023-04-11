8497e3118a378c352fb370ec5322d69f8340e948
Segimara <76587473+Segimara@users.noreply.github.com>
2023-04-11 12:41:19 +0300

    tests for website parser

---
+++ WebsiteTester.Tests/Parsers/WebsiteParserTests.cs
@@ -0,0 +1,37 @@
+﻿using HtmlAgilityPack;
+using Moq;
+using WebsiteTester.Normalizers;
+using WebsiteTester.Parsers;
+using WebsiteTester.Services;
+using WebsiteTester.Validators;
+using Xunit;
+
+namespace WebsiteTester.Tests.Parsers
+{
+    public class WebsiteParserTests
+    {
+        Mock<ContentLoaderService> _contentLoaderService;
+        Mock<UrlValidator> _urlValidator;
+        Mock<UrlNormalizer> _urlNormalizer;
+
+        WebsiteParser _websiteParser;
+
+        public WebsiteParserTests()
+        {
+            HtmlWeb htmlWeb = new HtmlWeb();
+
+            _contentLoaderService = new Mock<ContentLoaderService>(htmlWeb);
+            _urlValidator = new Mock<UrlValidator>();
+            _urlNormalizer = new Mock<UrlNormalizer>();
+
+            _websiteParser =
+                new WebsiteParser(_urlValidator.Object, _urlNormalizer.Object, _contentLoaderService.Object);
+        }
+
+        [Fact]
+        public void Parse_WhenUrlIsNull_ShouldReturnEmptyList()
+        {
+            Assert.Throws<ArgumentNullException>(() => _websiteParser.Parse(null));
+        }
+    }
+}
