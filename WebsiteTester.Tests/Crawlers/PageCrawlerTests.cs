0febfb1265fa83ecb4aa9be14a9c51f4e7348ab1
Segimara <76587473+Segimara@users.noreply.github.com>
2023-04-11 12:40:19 +0300

    tests for page crawler

--- WebsiteTester.Tests/Crawlers/PageCrawlerTests.cs
+++ WebsiteTester.Tests/Crawlers/PageCrawlerTests.cs
@@ -1,9 +1,5 @@
-﻿using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HtmlAgilityPack;
+using Moq;
 using WebsiteTester.Crawlers;
 using WebsiteTester.Normalizers;
 using WebsiteTester.Parsers;
@@ -15,7 +11,7 @@ namespace WebsiteTester.Tests.Crawlers
 {
     public class PageCrawlerTests
     {
-        private readonly Mock<ContentLoaderService> _httpClientService;
+        private readonly Mock<ContentLoaderService> _contentLoaderService;
         private readonly Mock<UrlNormalizer> _urlNormalizer;
         private readonly Mock<UrlValidator> _urlValidator;
         private readonly Mock<WebsiteParser> _websiteParser;
@@ -24,13 +20,16 @@ namespace WebsiteTester.Tests.Crawlers
 
         public PageCrawlerTests()
         {
-            var httpClient = new HttpClient();
+            var htmlWeb = new HtmlWeb();
 
-            _httpClientServiceMoq = new Mock<HttpClientService>(httpClient);
+            _contentLoaderService = new Mock<ContentLoaderService>(htmlWeb);
             _urlNormalizer = new Mock<UrlNormalizer>();
             _urlValidator = new Mock<UrlValidator>();
 
-            _pageCrawler = new PageCrawler();
+            _websiteParser = new Mock<WebsiteParser>(_urlValidator.Object, _urlNormalizer.Object,
+                _contentLoaderService.Object);
+
+            _pageCrawler = new PageCrawler(_websiteParser.Object);
         }
 
         [Fact]
@@ -48,16 +47,41 @@ namespace WebsiteTester.Tests.Crawlers
         }
 
         [Fact]
-        public void Crawl_WhenUrlIsNotValid_ShouldReturnEmptyList()
-        {
-            var result = _pageCrawler.Crawl("skype:someSkype");
-            Assert.Empty(result);
-        }
-
-        [Fact]
         public void Crawl_WhenUrlIsValid_ShouldReturnListOfUrls()
         {
-            var result = _pageCrawler.Crawl("");
+            _websiteParser.Setup(w => w.Parse("https://jwt.io/"))
+                .Returns(new[]
+                    { "https://jwt.io/introduction", "https://jwt.io/libraries", "https://jwt.io/#debugger-io" });
+
+            _websiteParser.Setup(w => w.Parse("https://jwt.io/introduction"))
+                .Returns(new[] { "https://jwt.io", "https://jwt.io/libraries", "https://jwt.io/#debugger-io" });
+
+            _websiteParser.Setup(w => w.Parse("https://jwt.io/libraries"))
+                .Returns(new[] { "https://jwt.io", "https://jwt.io/introduction", "https://jwt.io/#debugger-io" });
+
+            _websiteParser.Setup(w => w.Parse("https://jwt.io/#debugger-io"))
+                .Returns(new[] { "https://jwt.io", "https://jwt.io/libraries", "https://jwt.io/introduction" });
+
+            _urlNormalizer.Setup(n =>
+                    n.NormalizeUrls(new[]
+                        {
+                            "https://jwt.io/", "https://jwt.io/introduction", "https://jwt.io/libraries",
+                            "https://jwt.io/#debugger-io"
+                        },
+                        "https://jwt.io/"))
+                .Returns(new[]
+                {
+                    "https://jwt.io", "https://jwt.io/introduction", "https://jwt.io/libraries",
+                    "https://jwt.io/#debugger-io"
+                });
+
+            _urlValidator.Setup(v => v.IsValid("https://jwt.io")).Returns(true);
+            _urlValidator.Setup(v => v.IsValid("https://jwt.io/introduction")).Returns(true);
+            _urlValidator.Setup(v => v.IsValid("https://jwt.io/libraries")).Returns(true);
+            _urlValidator.Setup(v => v.IsValid("https://jwt.io/#debugger-io")).Returns(true);
+
+            var result = _pageCrawler.Crawl("https://jwt.io/");
+
             Assert.NotEmpty(result);
         }
     }
