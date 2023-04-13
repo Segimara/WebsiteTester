8e031c68cb06c0210b5b793c4521d52c7ba2f4ba
Segimara <76587473+Segimara@users.noreply.github.com>
2023-04-13 21:03:20 +0300

    [Feature] Add DI for Persistense Layer

---
+++ WebsiteTester.Persistense/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebsiteTester.Common.Interfaces;
+
+namespace WebsiteTester.Persistense
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddWebsiteTesterPersistense(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+
+            services.AddDbContext<WebsiteTesterDbContext>(options =>
+                           options.UseSqlite(connectionString));
+
+            services.AddScoped<IWebsiteTesterDbContext>(provider =>
+                provider.GetService<WebsiteTesterDbContext>());
+
+            return services;
+        }
+    }
+}
