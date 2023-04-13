2dabdae17ce00d9851c641fa319897cd88b40c48
Segimara <76587473+Segimara@users.noreply.github.com>
2023-04-13 21:04:35 +0300

    [Feature] Add configuration builder

--- WebsiteTester.Presentation/Program.cs
+++ WebsiteTester.Presentation/Program.cs
@@ -1,20 +1,34 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebsiteTester.Persistense;
 namespace WebsiteTester.Presentation
 {
     public class Program
     {
         static async Task Main(string[] args)
         {
+            var configuration = GetConfig();
+
             var services = new ServiceCollection();
 
             services.AddWebsiteTesterLogic();
             services.AddWebsiteTesterPresentation();
+
+            services.AddWebsiteTesterPersistense(configuration);
 
             var servicesProvider = services.BuildServiceProvider();
 
             await servicesProvider.GetService<WebsiteTesterApplication>().RunAsync();
         }
 
+        private static IConfiguration GetConfig()
+        {
+            return new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
+                {"DbConnection", $"Data Source = WebsiteTester.db"}
+            })
+            .Build();
+        }
     }
 }
