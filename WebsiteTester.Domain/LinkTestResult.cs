660d5e6d91e845234cc5a0162ae63ef8aca5e0e8
Segimara <76587473+Segimara@users.noreply.github.com>
2023-04-13 21:02:03 +0300

    [Feature] Edited Models

--- WebsiteTester.Domain/LinkTestResult.cs
+++ WebsiteTester.Domain/LinkTestResult.cs
@@ -2,5 +2,15 @@
 {
     public class LinkTestResult
     {
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public string Url { get; set; }
+        public int RenderTimeMilliseconds { get; set; }
+        public bool IsInSitemap { get; set; }
+        public bool IsInWebsite { get; set; }
+
+        public string TestedLinkId { get; set; }
+        public TestedLink TestedLink { get; set; }
+
     }
 }
--- WebsiteTester.Domain/TestedLink.cs
+++ WebsiteTester.Domain/TestedLink.cs
@@ -2,8 +2,7 @@
 {
     public class TestedLink
     {
-        public Guid Id { get; set; }
         public string Url { get; set; }
-        public IEnumerable<LinkTestResult> Links { get; set; }
+        public ICollection<LinkTestResult> Links { get; set; }
     }
 }
