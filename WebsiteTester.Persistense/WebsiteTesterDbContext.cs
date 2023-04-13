c60bd684325a79e9026dc74e5fdf8260d25368c2
Segimara <76587473+Segimara@users.noreply.github.com>
2023-04-13 21:05:43 +0300

    [Feature] Added DB context

--- WebsiteTester.Persistense/WebsiteTesterDbContext.cs
+++ WebsiteTester.Persistense/WebsiteTesterDbContext.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebsiteTester.Common.Interfaces;
+using WebsiteTester.Domain;
+using WebsiteTester.Persistense.Configurations;
 
 namespace WebsiteTester.Persistense
 {
-    public class WebsiteTesterDbContext : DbContext
+    public class WebsiteTesterDbContext : DbContext, IWebsiteTesterDbContext
     {
-        public DbSet<TestedLink>
+        public DbSet<TestedLink> TestedLink { get; set; }
+        public DbSet<LinkTestResult> LinkTestResult { get; set; }
+        public WebsiteTesterDbContext(DbContextOptions<WebsiteTesterDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TestedLinkConfiguration());
+            modelBuilder.ApplyConfiguration(new LinkTestResultConfiguration());
+        }
     }
 }
---
+++ WebsiteTester/Common/Interfaces/IWebsiteTesterDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebsiteTester.Domain;
+
+namespace WebsiteTester.Common.Interfaces
+{
+    public interface IWebsiteTesterDbContext
+    {
+        DbSet<TestedLink> TestedLink { get; set; }
+        DbSet<LinkTestResult> LinkTestResult { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
