ac227025453f7f2b1f7b9785f86a8325133de6f9
Segimara <76587473+Segimara@users.noreply.github.com>
2023-04-13 15:06:11 +0300

    [Refactor] refactor DI and rename Run method to RunAsync

--- WebsiteTester.Presentation/Program.cs
+++ WebsiteTester.Presentation/Program.cs
@@ -1,12 +1,4 @@
-﻿using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using HtmlAgilityPack;
-using Microsoft.Extensions.DependencyInjection;
-using WebsiteTester.Crawlers;
-using WebsiteTester.Normalizers;
-using WebsiteTester.Parsers;
-using WebsiteTester.Services;
-using WebsiteTester.Validators;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace WebsiteTester.Presentation
 {
@@ -14,39 +6,15 @@ namespace WebsiteTester.Presentation
     {
         static async Task Main(string[] args)
         {
-            var builder = new ContainerBuilder();
-
-            builder.Populate(ConfigureServices());
-
-            var servicesContainer = builder.Build();
-
-            await servicesContainer.Resolve<WebsiteTesterApplication>().Run();
-        }
-        private static IServiceCollection ConfigureServices()
-        {
             var services = new ServiceCollection();
 
+            services.AddWebsiteTesterLogic();
+            services.AddWebsiteTesterPresentation();
 
-            services.AddTransient<HtmlWeb>();
-            services.AddTransient<HttpClient>();
-            services.AddTransient<UrlValidator>();
-            services.AddTransient<UrlNormalizer>();
+            var servicesProvider = services.BuildServiceProvider();
 
-            services.AddTransient<ContentLoaderService>();
-            services.AddTransient<HttpClientService>();
-
-            services.AddTransient<WebsiteParser>();
-            services.AddTransient<SitemapParser>();
-            services.AddTransient<TimeMeterService>();
-
-            services.AddTransient<PageCrawler>();
-            services.AddTransient<WebsiteCrawler>();
-
-            services.AddTransient<ConsoleManager>();
-
-            services.AddTransient<WebsiteTesterApplication>();
-
-            return services;
+            await servicesProvider.GetService<WebsiteTesterApplication>().RunAsync();
         }
+
     }
 }
