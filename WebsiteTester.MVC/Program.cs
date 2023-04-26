004c97f9e8c2dc9440dec198e8f8b65b202a94fe
Segimara <76587473+Segimara@users.noreply.github.com>
2023-04-26 11:34:15 +0300

    [Feature] add loging to mvc

--- WebsiteTester.MVC/Program.cs
+++ WebsiteTester.MVC/Program.cs
@@ -9,6 +9,15 @@ namespace WebsiteTester.MVC
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                .AddConsole()
+                .SetMinimumLevel(LogLevel.Trace);
+            });
+
+            builder.Services.AddSingleton(loggerFactory.CreateLogger("Program"));
+
             builder.Services.AddControllersWithViews();
             builder.Services.AddWebsiteTesterLogic();
             builder.Services.AddWebsiteTesterPersistence(Environment.GetEnvironmentVariable("DB_CONNECTION"));
