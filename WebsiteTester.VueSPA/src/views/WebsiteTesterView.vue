f3f204ce83f66203eb221be0674f3e050bffb249
Segimara <76587473+Segimara@users.noreply.github.com>
2023-05-10 10:45:11 +0300

    [Feature] added normal date output

--- WebsiteTester.VueSPA/src/views/WebsiteTesterView.vue
+++ WebsiteTester.VueSPA/src/views/WebsiteTesterView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="d-flex flex-row gap-3 mt-5">
+    <div class="d-flex flex-row gap-3 mt-0">
         <div class="form-group d-flex align-items-center flex-row w-100">
             <label class="w-25">Enter a website</label>
             <input v-model="formUrl" type="text" class="form-control" id="url" name="url" placeholder="Enter URL">
@@ -25,7 +25,9 @@
             <tbody>
                 <tr v-for="link in links" :key="link.id">
                     <td>{{ link.url }}</td>
-                    <td>{{ link.createdOn }}</td>
+                    <td>{{ new Date(link.createdOn).toLocaleTimeString('default', { hour: '2-digit', minute: '2-digit' }) + " " +
+                        new Date(link.createdOn).toLocaleDateString('default', { day: '2-digit', month: '2-digit', year: 'numeric' })
+                    }}</td>
                     <td>
                         <RouterLink :to="{ name: 'TestDetails', params: { id: link.id } }">see details</RouterLink>
                     </td>
@@ -39,7 +41,6 @@
 import { defineComponent } from 'vue';
 import { useWebsiteTesterStore } from '../stores/WebsiteTesterStore';
 import { computed, ref } from '@vue/reactivity';
-
 export default defineComponent({
 
     setup() {
