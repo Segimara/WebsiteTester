0f7cd31cbec7b08926e2e61e634dec71b94cec86
Segimara <vadymgdm@gmail.com>
2023-09-06 09:35:15 +0300

    swap

--- WebsiteTesterCleanAchitecture/WebsiteTester.WebApi/Program.cs
+++ WebsiteTesterCleanAchitecture/WebsiteTester.WebApi/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using WebsiteTester.Infrastructure;
+using WebsiteTester.WebApi.TokenValidators;
 
 namespace WebsiteTester.WebApi
 {
@@ -17,7 +23,25 @@ namespace WebsiteTester.WebApi
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                AddSwaggerOAuth2Configuration(c);
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "wellship_svc_app", Version = "v1" });
+            });
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            })
+            .AddJwtBearer(o =>
+            {
+                o.IncludeErrorDetails = true;
+                o.SecurityTokenValidators.Clear();
+                o.SecurityTokenValidators.Add(new GoogleTokenValidator());
+            });
 
             builder.Services.AddCors(options =>
             {
@@ -40,10 +64,17 @@ namespace WebsiteTester.WebApi
             app.UseDeveloperExceptionPage();
 
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(c =>
+            {
+                c.OAuthAdditionalQueryStringParams(new Dictionary<string, string> { { "redirect_url", "https://localhost:44324/swagger/oauth2-redirect.html" } });
+                c.OAuthClientId("1063110563568-vr83cpjlcfpsbmvofr6doch2p1oiqor1.apps.googleusercontent.com");
+                c.OAuthClientSecret("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");
+                c.InjectJavascript("swaggerGoogleAuth.js");
+            });
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseCors("AllowAll");
@@ -51,6 +82,48 @@ namespace WebsiteTester.WebApi
             app.MapControllers();
 
             app.Run();
+        }
+
+        private static void AddSwaggerOAuth2Configuration(SwaggerGenOptions swaggerGenOptions)
+        {
+
+            var securityScheme = new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows()
+                {
+                    Implicit = new OpenApiOAuthFlow()
+                    {
+                        AuthorizationUrl = new Uri("https://accounts.google.com/o/oauth2/v2/auth"),
+                        Scopes = new Dictionary<string, string> { { "email", "email" }, { "profile", "profile" } }
+                    }
+                },
+                Extensions = new Dictionary<string, IOpenApiExtension>
+                {
+                    {"x-tokenName", new OpenApiString("id_token")}
+                },
+            };
+
+            swaggerGenOptions.AddSecurityDefinition("Bearer", securityScheme);
+
+            var securityRequirements = new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new List<string> {"email", "profile"}
+                }
+            };
+
+            swaggerGenOptions.AddSecurityRequirement(securityRequirements);
         }
     }
 }
--- WebsiteTesterCleanAchitecture/WebsiteTester.WebApi/ViewModels/Link.cs
+++ WebsiteTesterCleanAchitecture/WebsiteTester.WebApi/ViewModels/Link.cs
@@ -2,14 +2,10 @@
 {
     public class Link
     {
-        public Link()
-        {
-            TestResults = new List<TestResult>();
-        }
-
+        public Guid UserId { get; set; }
         public Guid Id { get; init; }
         public string Url { get; init; }
         public DateTimeOffset CreatedOn { get; init; }
-        public IEnumerable<TestResult> TestResults { get; set; }
+        public IEnumerable<TestResult> TestResults { get; set; } = new List<TestResult>();
     }
 }
